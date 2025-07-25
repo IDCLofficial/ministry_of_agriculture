0224c6bfd697953a6a8db6de4b913071cd7f7153
jephthahdominic <stdominicjephthah63@gmail.com>
2025-07-25 17:46:26 +0200

    Worked on the website

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -9,7 +9,7 @@ import CTASection from "../components/CTASection";
 export default function ContactUsPage() {
   return (
     <main className="min-h-screen w-full bg-white flex flex-col">
-      <ContactHeroSection title="Contact Us" />
+      <ContactHeroSection title="Reach Out to Us" />
       <section className="w-full mx-auto py-8 md:py-20 px-4 md:px-12 flex flex-col gap-8 md:gap-10">
         <div className="flex flex-col md:flex-row gap-8 relative">
             <div className="w-full md:w-[50%]">
