473acf9e3c8b501435fc38e8a9416a7a57ae2420
Joshua-Ogbonna <devjaykes@gmail.com>
2025-09-03 11:18:37 +0100

    chore: work

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps : [{
+    script: 'npm start',
+  }],
+
+  deploy : {
+    production : {
+      key: 'agric.pem',
+      user : 'ubuntu',
+      host : 'agric',
+      ref  : 'origin/main',
+      repo : 'git@github.com:IDCLofficial/ministry_of_agriculture.git',
+      path : '/home/ubuntu',
+      'pre-deploy-local': '',
+      'post-deploy' : 'source ~/.nvm/nvm.sh && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+      'ssh_options': 'ForwardAgent=yes'
+    }
+  }
+};
